36376e6813339b4c3154927c6f82ac735862acda
Alex Wandres <alex.r.wandres@gmail.com>
2021-02-28 12:26:51 -0500

    added provider

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,7 @@ import 'package:authentication/providers/auth.dart';
 import 'package:authentication/screens/login_screen.dart';
 import 'package:authentication/screens/splash_screen.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/cupertino.dart';
 import 'dart:io';
-
 
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -36,73 +34,73 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider.value(
           value: Auth(),
         ),
+        ChangeNotifierProvider.value(
+          value: SignupProvider(),
+        )
       ],
       child: Consumer<Auth>(
-        builder: (ctx,auth, _) =>
+        builder: (ctx, auth, _) =>
 //-------- Cupertino app renders if platform is IOS-----------------
-          //Platform.isIOS? CupertinoApp(
-          //   title: 'Storegrounds',
-          //   theme: CupertinoThemeData(
-          //     primarySwatch: Colors.purple,
-          //     errorColor: Colors.red,
-          //     accentColor: Colors.amber,
-          //     fontFamily: 'Quicksand',
-          //     textTheme: ThemeData.light().textTheme.copyWith(
-          //         title: TextStyle(
-          //             fontFamily: 'OpenSans',
-          //             fontWeight: FontWeight.bold,
-          //             fontSize: 18),
-          //         button: TextStyle(color: Colors.white)),
-          //     appBarTheme: AppBarTheme(
-          //         textTheme: ThemeData.light().textTheme.copyWith(
-          //             title: TextStyle(
-          //                 fontFamily: 'OpenSans',
-          //                 fontSize: 20,
-          //                 fontWeight: FontWeight.bold))
-          //     )
-          //   ),
-          //   home: auth.isAuth
-          //   ? CourierCentral()
-          //   : FutureBuilder(
-          //       future: auth.tryAutoLogin(),
-          //       builder: (ctx, authResultSnapshot) =>
-          //         authResultSnapshot.connectionState ==
-          //               ConnectionState.waiting
-          //           ? SplashScreen()
-          //           : LoginScreen(),
-          //     ),
-          // )
-          // :
+            //Platform.isIOS? CupertinoApp(
+            //   title: 'Storegrounds',
+            //   theme: CupertinoThemeData(
+            //     primarySwatch: Colors.purple,
+            //     errorColor: Colors.red,
+            //     accentColor: Colors.amber,
+            //     fontFamily: 'Quicksand',
+            //     textTheme: ThemeData.light().textTheme.copyWith(
+            //         title: TextStyle(
+            //             fontFamily: 'OpenSans',
+            //             fontWeight: FontWeight.bold,
+            //             fontSize: 18),
+            //         button: TextStyle(color: Colors.white)),
+            //     appBarTheme: AppBarTheme(
+            //         textTheme: ThemeData.light().textTheme.copyWith(
+            //             title: TextStyle(
+            //                 fontFamily: 'OpenSans',
+            //                 fontSize: 20,
+            //                 fontWeight: FontWeight.bold))
+            //     )
+            //   ),
+            //   home: auth.isAuth
+            //   ? CourierCentral()
+            //   : FutureBuilder(
+            //       future: auth.tryAutoLogin(),
+            //       builder: (ctx, authResultSnapshot) =>
+            //         authResultSnapshot.connectionState ==
+            //               ConnectionState.waiting
+            //           ? SplashScreen()
+            //           : LoginScreen(),
+            //     ),
+            // )
+            // :
 //-------- Material app renders if platform is Android-----------------
-          MaterialApp(
-            title: 'Storegrounds',
-            debugShowCheckedModeBanner: false,
-            theme: ThemeData(
+            MaterialApp(
+          title: 'Storegrounds',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
               fontFamily: 'Quicksand',
               primarySwatch: Colors.blue,
               visualDensity: VisualDensity.adaptivePlatformDensity,
               primaryColor: Color.fromRGBO(255, 195, 1, 1),
               textTheme: ThemeData.light().textTheme.copyWith(
-                title: TextStyle(
-                  fontSize: 29,
-                  fontFamily: 'Pacifico',
-                  fontWeight: FontWeight.bold
-                )
-              )
-            ),
+                  title: TextStyle(
+                      fontSize: 29,
+                      fontFamily: 'Pacifico',
+                      fontWeight: FontWeight.bold))),
           home: auth.isAuth
-          ? CourierCentral()
-          : FutureBuilder(
-              future: auth.tryAutoLogin(),
-              builder: (ctx, authResultSnapshot) =>
-                authResultSnapshot.connectionState ==
-                      ConnectionState.waiting
-                  ? SplashScreen()
-                  : LoginScreen(),
-            ),
-            // routes: 
-            // 'sign-up': (ctx)=>SignUpScreen(),
-          ),
+              ? CourierCentral()
+              : FutureBuilder(
+                  future: auth.tryAutoLogin(),
+                  builder: (ctx, authResultSnapshot) =>
+                      authResultSnapshot.connectionState ==
+                              ConnectionState.waiting
+                          ? SplashScreen()
+                          : LoginScreen(),
+                ),
+          // routes:
+          // 'sign-up': (ctx)=>SignUpScreen(),
+        ),
       ),
     );
   }
