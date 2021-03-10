36b2130378efde719fa2d8a4e9dbbf7d16598e4c
Subaiyal Sheikh <subaiyal@Subaiyals-Mini.attlocal.net>
2021-03-09 18:49:05 -0600

    added bottom navbar

--- lib/delivery_dashboard/screens/deliverydashboard.dart
+++ lib/delivery_dashboard/screens/deliverydashboard.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:storegrounds/delivery_dashboard/screens/page1.dart';
+import 'package:storegrounds/delivery_dashboard/screens/page2.dart';
+import 'package:storegrounds/delivery_dashboard/screens/page3.dart';
 import '../../global/widgets/custom_drawer.dart';
 
 class DeliveryDashboard extends StatefulWidget {
@@ -10,6 +13,21 @@ class _DeliveryDashboardState extends State<DeliveryDashboard>
     with SingleTickerProviderStateMixin {
   TabController _tabController;
   PageController _pageController;
+
+  final List<Widget> _pages =[
+    PageOne(),
+    PageTwo(),
+    PageThree()
+
+  ];
+
+  int _selectedPageIndex =0;
+
+  void _selectedPage(int index){
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +45,32 @@ class _DeliveryDashboardState extends State<DeliveryDashboard>
         ),
       ),
       drawer: CustomDrawer(),
-      body: Text('USER DASH'),
+      body: _pages[_selectedPageIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectedPage ,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedPageIndex,
+        type: BottomNavigationBarType.shifting,
+        items: [
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.storefront), 
+            title: Text('Jobs')
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.grid_view), 
+            title: Text('Main')
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.location_on), 
+            title: Text('Location')
+          ),
+        ],
+      ),
     );
   }
 }
