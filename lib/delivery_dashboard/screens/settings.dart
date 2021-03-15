4e710978cebc2f7570ca4463a558ed4ddf523109
Erikk Shupp <local@tradegrounds.info>
2021-03-15 03:29:54 +0000

    refactored the feature/delivery_dashboard and created blank widget files for the build of the scaffold via main.dart - changed all "courier central" to delivery dashboard

---
+++ lib/delivery_dashboard/screens/settings.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        child: Center(child: Text('Settings Screen'),),
+      ),
+    );
+  }
+}
---
+++ lib/delivery_dashboard/widgets/productcenter.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProductCenterScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(child: Text('Product Center Screen'),),
+    );
+  }
+}
