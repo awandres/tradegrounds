366a45bc7ffb87e323180f4918c587154236138e
Subaiyal Sheikh <subaiyal@Subaiyals-Mini.attlocal.net>
2021-03-21 18:40:16 -0500

    added stack and optimzed for ipad and iphone x

--- lib/delivery_dashboard/widgets/deliveries.dart
+++ lib/delivery_dashboard/widgets/deliveries.dart
@@ -9,72 +9,77 @@ class Deliveries extends StatelessWidget {
     return Expanded(
         flex: 2,
         child: FittedBox(
-          fit: BoxFit.contain,
-          // padding: EdgeInsets.all(device.width/10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              FittedBox( 
-                fit: BoxFit.contain, 
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      FittedBox(
-                        fit: BoxFit.fill,
-                        child: Text(
-                          '13',
-                          style: TextStyle(
-                            color: Colors.green,
-
-                            fontFamily: 'Pacifico'
+          fit: BoxFit.fitWidth,
+          child: Container(
+            padding: device.width<800? EdgeInsets.all(20): EdgeInsets.all(40),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                // FittedBox( 
+                //   fit: BoxFit.fitHeight, 
+                    // child: 
+                    Stack(
+                      alignment: AlignmentDirectional.center,
+                      overflow: Overflow.visible,
+                      children: <Widget>[
+                        Positioned(
+                            child: Text('13',
+                            strutStyle: StrutStyle(
+                              forceStrutHeight: true,
+                            ),
+                            style: TextStyle(
+                              color: Colors.green,
+                              fontFamily: 'Pacifico',
+                              fontSize: 50
+                            ),
                           ),
                         ),
-                      ),
-                    
-                      FittedBox(
-                        fit: BoxFit.fill,
-                        child: Text(
-                          ' Deliveries',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold
+                        Positioned(
+                            bottom: -15,
+                            child: Text(
+                              'deliveries',
+                            style: TextStyle(
+                              fontFamily: 'Quicksand',
+                              fontSize: 10
+                            ),
                           ),
+                        )
+                      ],
+                    ),
+                // ),
+                SizedBox(width:30),
+                FittedBox(
+                  fit: BoxFit.contain,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: <Widget>[
+                      Text(
+                        'March 20th 2021',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Quicksand'
+                        ),
+                      ),
+                      Text(
+                        'Morning route',
+                        style: TextStyle(
+                          fontFamily: 'Pacifico'
+                        ),
+                      ),
+                      Text(
+                        'In Progress',
+                        style: TextStyle(
+                          fontFamily: 'Quicksand'
                         ),
                       ),
                     ],
-                ),
-              ),
-              FittedBox(
-                fit: BoxFit.contain,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: <Widget>[
-                    Text(
-                      'March 20th 2021',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'Quicksand'
-                      ),
-                    ),
-                    Text(
-                      'Morning route',
-                      style: TextStyle(
-                        fontFamily: 'Pacifico'
-                      ),
-                    ),
-                    Text(
-                      'In Progress',
-                      style: TextStyle(
-                        fontFamily: 'Quicksand'
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       );
