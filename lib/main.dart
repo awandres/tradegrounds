faca4f5faa766f1edaeef9c24b4fbc378763098b
Alex Wandres <alex.r.wandres@gmail.com>
2021-03-23 12:22:28 -0400

    commented out development code on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -29,110 +29,110 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    // return MultiProvider(
-    //   providers: [
-    //     ChangeNotifierProvider.value(
-    //       value: Auth(),
-    //     ),
-    //     ChangeNotifierProvider.value(
-    //       value: SignupProvider(),
-    //     )
-    //   ],
-    //   child: Consumer<Auth>(
-    //     builder: (ctx, auth, _) =>
-//-------- Cupertino app renders if platform is IOS-----------------
-    //Platform.isIOS? CupertinoApp(
-    //   title: 'Storegrounds',
-    //   theme: CupertinoThemeData(
-    //     primarySwatch: Colors.purple,
-    //     errorColor: Colors.red,
-    //     accentColor: Colors.amber,
-    //     fontFamily: 'Quicksand',
-    //     textTheme: ThemeData.light().textTheme.copyWith(
-    //         title: TextStyle(
-    //             fontFamily: 'OpenSans',
-    //             fontWeight: FontWeight.bold,
-    //             fontSize: 18),
-    //         button: TextStyle(color: Colors.white)),
-    //     appBarTheme: AppBarTheme(
-    //         textTheme: ThemeData.light().textTheme.copyWith(
-    //             title: TextStyle(
-    //                 fontFamily: 'OpenSans',
-    //                 fontSize: 20,
-    //                 fontWeight: FontWeight.bold))
-    //     )
-    //   ),
-    //   home: auth.isAuth
-    //   ? CourierCentral()
-    //   : FutureBuilder(
-    //       future: auth.tryAutoLogin(),
-    //       builder: (ctx, authResultSnapshot) =>
-    //         authResultSnapshot.connectionState ==
-    //               ConnectionState.waiting
-    //           ? SplashScreen()
-    //           : LoginScreen(),
-    //     ),
-    // )
-    // :
-//-------- Material app renders if platform is Android-----------------
-    //         MaterialApp(
-    //       title: 'Storegrounds',
-    //       debugShowCheckedModeBanner: false,
-    //       theme: ThemeData(
-    //           fontFamily: 'Quicksand',
-    //           primarySwatch: Colors.blue,
-    //           visualDensity: VisualDensity.adaptivePlatformDensity,
-    //           primaryColor: Color.fromRGBO(255, 195, 1, 1),
-    //           textTheme: ThemeData.light().textTheme.copyWith(
-    //               title: TextStyle(
-    //                   fontSize: 29,
-    //                   fontFamily: 'Pacifico',
-    //                   fontWeight: FontWeight.bold))),
-    //       home: auth.isAuth
-    //           ? DeliveryDashboard()
-    //           : FutureBuilder(
-    //               future: auth.tryAutoLogin(),
-    //               builder: (ctx, authResultSnapshot) =>
-    //                   authResultSnapshot.connectionState ==
-    //                           ConnectionState.waiting
-    //                       ? SplashScreen()
-    //                       : LoginScreen(),
-    //             ),
-    //       // routes:
-    //       // 'sign-up': (ctx)=>SignUpScreen(),
-    //     ),
-    //   ),
-    // );
-
-    // USE FOR DEVELOPMENT //
-    //
-    return ChangeNotifierProvider(
-      create: (context) => SignupProvider(),
-      child: MaterialApp(
-        title: 'Storegrounds',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          fontFamily: 'Quicksand',
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-          primaryColor: Color.fromRGBO(255, 195, 1, 1),
-          textTheme: ThemeData.light().textTheme.copyWith(
-              title: TextStyle(
-                  fontSize: 29,
-                  fontFamily: 'Pacifico',
-                  fontWeight: FontWeight.bold)),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: Auth(),
         ),
-        home: StreamBuilder(
-          stream: FirebaseAuth.instance.onAuthStateChanged,
-          builder: (ctx, userSnapshot) {
-            if (userSnapshot.hasData) {
-              return DeliveryDashboard();
-            }
-            return LoginScreen();
-          },
+        ChangeNotifierProvider.value(
+          value: SignupProvider(),
+        )
+      ],
+      child: Consumer<Auth>(
+        builder: (ctx, auth, _) =>
+//-------- Cupertino app renders if platform is IOS-----------------
+            //Platform.isIOS? CupertinoApp(
+            //   title: 'Storegrounds',
+            //   theme: CupertinoThemeData(
+            //     primarySwatch: Colors.purple,
+            //     errorColor: Colors.red,
+            //     accentColor: Colors.amber,
+            //     fontFamily: 'Quicksand',
+            //     textTheme: ThemeData.light().textTheme.copyWith(
+            //         title: TextStyle(
+            //             fontFamily: 'OpenSans',
+            //             fontWeight: FontWeight.bold,
+            //             fontSize: 18),
+            //         button: TextStyle(color: Colors.white)),
+            //     appBarTheme: AppBarTheme(
+            //         textTheme: ThemeData.light().textTheme.copyWith(
+            //             title: TextStyle(
+            //                 fontFamily: 'OpenSans',
+            //                 fontSize: 20,
+            //                 fontWeight: FontWeight.bold))
+            //     )
+            //   ),
+            //   home: auth.isAuth
+            //   ? CourierCentral()
+            //   : FutureBuilder(
+            //       future: auth.tryAutoLogin(),
+            //       builder: (ctx, authResultSnapshot) =>
+            //         authResultSnapshot.connectionState ==
+            //               ConnectionState.waiting
+            //           ? SplashScreen()
+            //           : LoginScreen(),
+            //     ),
+            // )
+            // :
+//-------- Material app renders if platform is Android-----------------
+            MaterialApp(
+          title: 'Storegrounds',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+              fontFamily: 'Quicksand',
+              primarySwatch: Colors.blue,
+              visualDensity: VisualDensity.adaptivePlatformDensity,
+              primaryColor: Color.fromRGBO(255, 195, 1, 1),
+              textTheme: ThemeData.light().textTheme.copyWith(
+                  title: TextStyle(
+                      fontSize: 29,
+                      fontFamily: 'Pacifico',
+                      fontWeight: FontWeight.bold))),
+          home: auth.isAuth
+              ? DeliveryDashboard()
+              : FutureBuilder(
+                  future: auth.tryAutoLogin(),
+                  builder: (ctx, authResultSnapshot) =>
+                      authResultSnapshot.connectionState ==
+                              ConnectionState.waiting
+                          ? SplashScreen()
+                          : LoginScreen(),
+                ),
+          // routes:
+          // 'sign-up': (ctx)=>SignUpScreen(),
         ),
       ),
     );
+
+    // USE FOR DEVELOPMENT //
+    //
+    // return ChangeNotifierProvider(
+    //   create: (context) => SignupProvider(),
+    //   child: MaterialApp(
+    //     title: 'Storegrounds',
+    //     debugShowCheckedModeBanner: false,
+    //     theme: ThemeData(
+    //       fontFamily: 'Quicksand',
+    //       primarySwatch: Colors.blue,
+    //       visualDensity: VisualDensity.adaptivePlatformDensity,
+    //       primaryColor: Color.fromRGBO(255, 195, 1, 1),
+    //       textTheme: ThemeData.light().textTheme.copyWith(
+    //           title: TextStyle(
+    //               fontSize: 29,
+    //               fontFamily: 'Pacifico',
+    //               fontWeight: FontWeight.bold)),
+    //     ),
+    //     home: StreamBuilder(
+    //       stream: FirebaseAuth.instance.onAuthStateChanged,
+    //       builder: (ctx, userSnapshot) {
+    //         if (userSnapshot.hasData) {
+    //           return DeliveryDashboard();
+    //         }
+    //         return LoginScreen();
+    //       },
+    //     ),
+    //   ),
+    // );
 
     // END DEVELOPMENT
   }
