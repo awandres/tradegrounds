105f02480947c0b1c8c8741fe63684b7d8d6ba5e
Subaiyal Sheikh <subaiyal@Subaiyals-Mini.attlocal.net>
2021-03-31 08:50:28 -0500

    order creation widget 50% done

--- lib/delivery_dashboard/widgets/mainscreen.dart
+++ lib/delivery_dashboard/widgets/mainscreen.dart
@@ -24,22 +24,78 @@ class MainScreen extends StatelessWidget {
               ),
               Expanded(
                 flex: 2,
-                child: Container(
-                  color: Colors.redAccent,
-                  child: Row(
-                    children: <Widget>[
-                      Text(
-                        'deliveries',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      Text(
-                        'morning route',
-                        style: TextStyle(color: Colors.white),
-                      )
-                    ],
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 25, right: 25),
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                            child: Container(
+                              margin: EdgeInsets.only(bottom:30, left:20),
+                              child: Material(
+                                elevation: 10,
+                                type: MaterialType.card,
+                                borderRadius: BorderRadius.all(Radius.circular(10)),
+                                child: Stack(
+                                  alignment: AlignmentDirectional.center,
+                                  fit: StackFit.expand,
+                                  children: <Widget>[
+                                    FittedBox( fit: BoxFit.contain ,child: Padding(
+                                      padding: const EdgeInsets.all(18.0),
+                                      child: Center(
+                                        child: Text.rich(
+                                        TextSpan(
+                                          text:'\$1900\n', 
+                                          style: TextStyle(fontSize:13,color:Color(0xFF009A9A),),
+                                          children: <TextSpan>[
+                                            TextSpan(
+                                              text:'Estimate', 
+                                              style: TextStyle(
+                                                fontSize:10,color:Colors.black,
+                                              ),
+                                            )
+                                            ],
+                                          ),
+                                        )
+                                      )
+                                    )),
+                                    Positioned(
+                                      right:12,
+                                      top: 12,
+                                      child: Icon(
+                                        Icons.lock,
+                                        color: Colors.grey,
+                                        size: 24.0,
+                                        semanticLabel: 'Text to announce in accessibility modes',
+                                      ),
+                                    ),
+                                    
+                                  ]
+                                ),
+                              ),
+                            ),
+                          
+                        ),
+                        SizedBox(width:50),
+                        Expanded(
+                          child: FittedBox(
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                IconButton(
+                                  icon: const Icon(Icons.add),
+                                  tooltip: 'Increase volume by 10',
+                                ),
+                                Icon(Icons.circle, ),
+                                Text('New Order')
+                              ],
+                            )
+                          )
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
+            
           ]),
     ));
   }
